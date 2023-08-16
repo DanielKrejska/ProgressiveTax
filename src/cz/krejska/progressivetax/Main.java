7d9f7ea3670d18118d760a87fbda7ec4ac2b2c69
Daniel Krejska <dankrejska@seznam.cz>
2023-08-16 11:26:30 +0200

    v0.1.2 -formatting fixed

--- src/cz/krejska/progressivetax/Main.java
+++ src/cz/krejska/progressivetax/Main.java
@@ -4,8 +4,10 @@ package cz.krejska.progressivetax;
  * @author Daniel Krejska
  * @since 15.8.2023
  */
-final class Main {
-    public static void main(String[] args) {
+final class Main
+{
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
     }
 }
